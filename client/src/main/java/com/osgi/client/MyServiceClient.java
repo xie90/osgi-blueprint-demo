741c821a1d90b8095d43d934b0e45569a70f4f54
xie90 <15280047006@139.com>
2020-02-19 16:17:15 +0800

    use static

--- client/src/main/java/com/osgi/client/MyServiceClient.java
+++ client/src/main/java/com/osgi/client/MyServiceClient.java
@@ -1,5 +1,6 @@
 package com.osgi.client;
 
+import com.osgi.impl.WorldImpl;
 import com.osgi.inter.Hello;
 
 public class MyServiceClient {
@@ -16,6 +17,7 @@ public class MyServiceClient {
     public void doTest(){
         System.out.println("client start test");
         hello.sayHello("hello world");
+        WorldImpl.sayWorld("Hello World Again");
         System.out.println("client test end");
     }
 
---
+++ server/src/main/java/com/osgi/impl/WorldImpl.java
@@ -0,0 +1,12 @@
+package com.osgi.impl;
+
+public class WorldImpl {
+
+    public static void sayWorld(String word) {
+        System.out.println("say : "+ word);
+    }
+
+    public static void startup(){
+        System.out.println("World Start Up");
+    }
+}
